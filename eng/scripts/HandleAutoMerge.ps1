43b012d677ac7f927bb65f2b8a64cb6d11c33944
Wes Haggard <Wes.Haggard@microsoft.com>
2022-11-15 13:43:00 -0800

    Add handle auto-merge script

---
+++ eng/scripts/HandleAutoMerge.ps1
@@ -0,0 +1,30 @@
+[CmdletBinding()]
+param(
+  [Parameter(Mandatory = $true)]
+  [string]$label,
+  [Parameter(Mandatory = $true)]
+  [string]$pullRequestId,
+  [string]$pullRequestNumber = $pullRequestId,
+  [switch]$labelAdded
+)
+Set-StrictMode -Version 3
+
+. $PSScriptRoot/Github-Project-Helpers.ps1
+
+Write-Verbose $(gh api -H "Accept: application/vnd.github.v3+json" /rate_limit --jq '.resources')
+
+if ($labelAdded -ne "auto-merge") {
+  Write-Host "Skipping action because the label name is [$labelAdded] which is not [auto-merge]."
+  exit 0
+}
+
+if ($labelAdded) {
+  Write-Host "Enabling auto merge for pull request $pullRequestNumber because [$label] was added."
+  EnablePullRequestAutoMerge $pullRequestId
+}
+else {
+  Write-Host "Disabling auto merge for pull request $pullRequestNumber because [$label] was removed."
+  DisablePullRequestAutoMerge $pullRequestId
+}
+
+Write-Verbose (gh api -H "Accept: application/vnd.github.v3+json" /rate_limit --jq '.resources')
